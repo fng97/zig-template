853ad58f0926dbea3b1beceb4851d353f669127e
Francisco Nevitt Gonçalves <git@francisco.wiki>
2026-03-03 21:44:07 +0000

    Add `tidy.zig` to tests

--- build.zig
+++ build.zig
@@ -7,17 +7,31 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     const test_step = b.step("test", "Run tests");
 
-    const exe = b.addExecutable(.{
-        .name = "zig_template",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-    b.installArtifact(exe);
-    run_step.dependOn(&b.addRunArtifact(exe).step);
+    const tidy_dep = b.dependency("tidy", .{ .target = b.graph.host, .optimize = .ReleaseSafe });
 
-    const exe_tests = b.addTest(.{ .root_module = exe.root_module });
-    test_step.dependOn(&b.addRunArtifact(exe_tests).step);
+    const mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    run_step.dependOn(blk: {
+        const exe = b.addExecutable(.{ .name = "zig_template", .root_module = mod });
+        b.installArtifact(exe);
+        const run = b.addRunArtifact(exe);
+        run.step.dependOn(b.getInstallStep()); // run from prefix
+        break :blk &run.step;
+    });
+
+    test_step.dependOn(blk: {
+        const exe = b.addTest(.{ .root_module = mod });
+        const run = b.addRunArtifact(exe);
+        break :blk &run.step;
+    });
+
+    test_step.dependOn(blk: {
+        const exe = b.addTest(.{ .name = "tidy checks", .root_module = tidy_dep.module("tidy") });
+        const run = b.addRunArtifact(exe);
+        break :blk &run.step;
+    });
 }
--- src/main.zig
+++ src/main.zig
@@ -17,7 +17,8 @@ test "fuzz example" {
     const Context = struct {
         fn testOne(context: @This(), input: []const u8) anyerror!void {
             _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test
+            // case!
             try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
         }
     };
